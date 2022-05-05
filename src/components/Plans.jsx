7a1903a2702657b7c6b43109c47fb4ca302e2b8b
Cesar Favero <cesarfaverodesouza@icloud.com>
2022-05-05 19:34:00 -0300

    Contatos atualizado, quase finalizado

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -3,6 +3,16 @@ import OkIcon from '../assets/images/ok.svg'
 import BrazilIcon from '../assets/images/brazil.png'
 import MoneyIcon from '../assets/images/money'
 
+import YouTube from '../assets/images/down/youtube.svg'
+import LinkedIn from '../assets/images/down/linkedin.svg'
+import WhatsApp from '../assets/images/down/whatsapp.svg'
+import Instagram from '../assets/images/down/instagram.svg'
+import Facebook from '../assets/images/down/facebook.svg'
+import Map from '../assets/images/down/map'
+import Mail from '../assets/images/down/mail'
+import Phone from '../assets/images/down/phone.png'
+
+
 export function Plans(){
   return(
   <div>
@@ -144,28 +154,69 @@ export function Plans(){
       
         <header>
         
-          <hr class="detail-plans"/>
+          <hr className="detail-plans"/>
           <p>Nossos Principais Orçamentos</p>
           
         </header>
+
+        {/*AREA DE CONTATOS (ICONES ETC)*/}
+      <div className="contacts-div">
+        <div className="white">
+          <div className="Map">
+            <img className="Map" src={Map}/>
+          </div>
+          <p>Quadra 108 Sul Alameda 8, 330 Bairro: Plano Diretor Sul</p>
+        </div>
         
-        <div className="map">
-
+        <div className="grey">
+          <div className="Facebook">
+            <img className="Facebook" src={Facebook}/>
+          </div>
+          <a>facebook.com/XPTO</a>
         </div>
-
-        <div className="facebook">
+        <div className="white">
+          <div className="YouTube">
+            <img className="YouTube" src={YouTube}/>
+          </div>
+          <a>youtube.com/XPTO</a>
+        </div>
+        <div className="grey">
+          <div className="LinkedIn">
+            <img className="LinkedIn" src={LinkedIn}/>
+          </div>
+          <a>linkedin.com/XPTO</a>
+        </div>
+        <div className="white">
+          <div className="Instagram">
+            <img className="insta-image" src={Instagram}/>
+          </div>
+          <a>instagram.com/XPTO</a>
+        </div>
+        <div className="grey">
+          <div className="WhatsApp">
+            <img className="WhatsApp" src={WhatsApp}/>
+          </div>
+          <a>whatsapp.com/XPTO</a>
+        </div>
+        <div className="white">
+          <div className="Phone">
+            <img className="Phone" src={Phone}/>
+          </div>
+          <p>+55 (12) 9386-7383</p>
+        </div>
+        <div className="grey">
+          <div className="Mail">
+            <img className="Mail" src={Mail}/>
+          </div>
+            <a>contato@XPTO.com</a>
+        </div>
+        
           
-        </div>
-
-        <div className="map">
-          
-        </div>
-
-        <div className="facebook">
-          
-        </div>
       </div>
-    </section>
+    </div>
+
+    <footer></footer>
+  </section>
 
   </div>
 </div>
